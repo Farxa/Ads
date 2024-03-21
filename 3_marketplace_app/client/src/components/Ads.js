166af05536690f6019e202613fdbb9cfbcc359bb
farxa <r.abualhassan@gmail.com>
2024-03-21 18:07:52 +0100

    Refactor Ads component to use async/await for fetching ads

--- 3_marketplace_app/client/src/components/Ads.js
+++ 3_marketplace_app/client/src/components/Ads.js
@@ -7,14 +7,27 @@ const Ads = () => {
   const dispatch = useAdsDispatch();
   const { ads } = useAdsState();
 
-  useEffect(() => {
-    fetch("http://localhost:8080/ads")
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch({ type: "SET_ADS", payload: data });
-      });
-  }, [dispatch]);
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/ads")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       dispatch({ type: "SET_ADS", payload: data });
+  //     });
+  // }, [dispatch]);
 
+  useEffect(() => {
+    const fetchAds = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/ads");
+        const data = await response.json();
+        dispatch({ type: "SET_ADS", payload: data });
+      } catch (error) {
+        console.error("Failed to fetch ads:", error);
+      }
+    };
+
+    fetchAds();
+  }, [dispatch]);
   const handleAdClick = (ad) => {
     dispatch({ type: "SET_SELECTED_AD", payload: ad });
   };
@@ -25,9 +38,8 @@ const Ads = () => {
       <div className="ads-list">
         {ads.map((ad) => (
           <div className="ad-card" key={ad.id}>
-            {" "}
             <img
-              src={ad.imageUrl}
+              src="https://as2.ftcdn.net/v2/jpg/05/17/53/57/1000_F_517535712_q7f9QC9X6TQxWi6xYZZbMmw5cnLMr279.jpg"
               alt={ad.title}
               onClick={() => handleAdClick(ad)}
             />
